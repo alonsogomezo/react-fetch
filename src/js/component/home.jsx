8317a8d772c4206f9d8230aa04d8bfb903c71a68
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-25 03:06:00 +0000

    fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,19 +10,24 @@ const Home = () => {
 	return (
 		<div>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<div className="card" style={{ width: "18rem" }}>
-				<img className="card-img-top" src="..." alt="Card image cap" />
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#" class="btn btn-primary">
-						Go somewheree
-					</a>
-				</div>
-			</div>
+			{pokemones.map((item, index) => (
+				<p key={index}>
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							className="card-img-top"
+							src="..."
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">{item.name}</h5>
+							<p className="card-text">jugas jugas xd</p>
+							<a href={item.url} class="btn btn-primary">
+								URL de la vara
+							</a>
+						</div>
+					</div>
+				</p>
+			))}
 		</div>
 	);
 };
