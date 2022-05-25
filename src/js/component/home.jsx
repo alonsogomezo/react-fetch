8779903f973316e9c237e63ee290b30d3fd929eb
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-25 02:18:00 +0000

    medias

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [pokemones, setPokemones] = useState([]);
+
+	fetch("https://pokeapi.co/api/v2/pokemon")
+		.then((resp) => resp.json())
+		.then((response) => setPokemones(response.results)) //results es el nombre del arreglo donde estan los pokemones, en el API
+		.catch();
 	return (
 		<div>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="card" style={{ width: "18rem" }}>
+				<img className="card-img-top" src="..." alt="Card image cap" />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewheree
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
